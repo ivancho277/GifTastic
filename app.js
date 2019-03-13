6e89d9478eb2cb997e6a61127bf8b5913026d956
ivancho277 <rouskoi@live.com>
2019-03-12 20:32:39 -0700

    buttons appearing dynamically

--- app.js
+++ app.js
@@ -4,11 +4,42 @@
 // var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
 // xhr.done(function(data) { console.log("success got data", data); });
 
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=Ohxt7aSzLjq13m7TKezI44mLur3VAS1z&limit=5"
+
+
+
+var animal = "";
+var buttonArray = [];
+$("#button-addon1").on("click", ()=> {
+    var newbutton = $("<button>")
+    newbutton.attr("id", "animal-btn")
+    animal = $("#gif-search").val();
+    newbutton.text(animal);
+
+    
+    buttonArray.push(newbutton);
+    
+    console.log(buttonArray);
+    console.log(newbutton);
+
+    for(var i=0; i<buttonArray.length; i++){
+        //$("#button-added").empty();
+        $("#button-added").append(buttonArray[i]);
+    }
+    
+
+
+})
+
+var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ animal +"&api_key=Ohxt7aSzLjq13m7TKezI44mLur3VAS1z&limit=5"
+
 
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response) {
     console.log(response);
+
+
+
+
 })
